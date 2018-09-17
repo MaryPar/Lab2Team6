65a16603be1efe52315b475abc0662508d8cb26c
JacobSiau <jacob.siau@ttu.edu>
2018-09-17 14:39:12 -0500

    Updates

--- PythonTest/Test.py
+++ PythonTest/Test.py
@@ -1,9 +1,15 @@
 import requests
 import json
+from pprint import pprint
 
-url = "http://172.16.0.1:8001/FieldData/GetData"
-r = requests.get(url)
-data = r.json()
-print(data)
-ball = data['Ball']
-print(ball)
+# url = "http://172.16.0.1:8001/FieldData/GetData"
+# r = requests.get(url)
+# data = r.json()
+# print(data)
+# ball = data['Ball']
+# print(ball)
+
+with open('FieldData.json') as f:
+    data = json.load(f)
+
+pprint(data)
