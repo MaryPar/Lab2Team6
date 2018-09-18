dfe87c7649a68e868b036bacc64737dda42654f5
JacobSiau <jacob.siau@ttu.edu>
2018-09-18 03:09:59 -0500

    Updated pixelmap scaling

--- PythonTest/Test.py
+++ PythonTest/Test.py
@@ -3,24 +3,40 @@ import json
 from pprint import pprint
 
 # url = "http://172.16.0.1:8001/FieldData/GetData"
-# r = requests.get(url)
-# data = r.json()
-# print(data)
-# ball = data['Ball']
-# print(ball)
+# f = requests.get(url)
 
-with open('FieldData.json') as f:
-    data = json.load(f)
+f = open('FieldData.json', 'r')
+data = json.load(f)
 
-print(data['Corners'])
+print("Original Object: " + str(data))
+
+# easier object names and access
+ball = (data['Ball']['Object Center']['X'], data['Ball']['Object Center']['Y'])
+corners = data['Corners']
+redteam = {
+    'circle': (data['Red Team Data']['Circle']['Object Center']['X'],
+               data['Red Team Data']['Circle']['Object Center']['Y']),
+    'square': (data['Red Team Data']['Square']['Object Center']['X'],
+               data['Red Team Data']['Square']['Object Center']['Y']),
+    'triangle': (data['Red Team Data']['Triangle']['Object Center']['X'],
+                 data['Red Team Data']['Triangle']['Object Center']['Y'])}
+blueteam = {
+    'circle': (data['Blue Team Data']['Circle']['Object Center']['X'],
+               data['Blue Team Data']['Circle']['Object Center']['Y']),
+    'square': (data['Blue Team Data']['Square']['Object Center']['X'],
+               data['Blue Team Data']['Square']['Object Center']['Y']),
+    'triangle': (data['Blue Team Data']['Triangle']['Object Center']['X'],
+                 data['Blue Team Data']['Triangle']['Object Center']['Y'])}
 
 xcoords = []
 ycoords = []
 
-for d in data['Corners']:
+# grabbing the x and y coordinates of the pairs in corners
+for d in corners:
     xcoords.append(d['X'])
     ycoords.append(d['Y'])
 
+# sort the coordinates ascending so we can find max, min, and range for x and y
 xcoords.sort()
 ycoords.sort()
 xmax = xcoords[3]
@@ -30,26 +46,19 @@ ymin = ycoords[0]
 xrange = xmax - xmin
 yrange = ymax - ymin
 
-print("coords: " + str(xcoords) + " "+ str(ycoords))
-print("max: " + str(xmax) + " " + str(ymax))
-print("min: " + str(xmin) + " " + str(ymin))
-print("range: " + str(xrange) + " " + str(yrange))
-
-# pixmapx maps a pixel value px to a real value in the range [0,10]
-def pixmapx(px):
-    return 10 - 10 * (xmax - px) / xrange
-
-# pixmapy maps a pixel value py to a real value in the range [0,10]
-def pixmapy(py):
-    return 10 - 10 * (ymax - py) / yrange
+print("Original Ball: " + str(ball))
+print("Coords: " + str(xcoords) + " "+ str(ycoords))
+print("Max (x,y): " + str(xmax) + " " + str(ymax))
+print("Min (x,y): " + str(xmin) + " " + str(ymin))
+print("Range (x,y): " + str(xrange) + " " + str(yrange))
 
 
-print("ball: " + str(data['Ball']))
-print("ballX: " + str(pixmapx(data['Ball']['Object Center']['X'])))
-print("ballY: " + str(pixmapy(data['Ball']['Object Center']['Y'])))
+# pixelmap maps a pixel pair (px, py) to a real coordinate pair (rx, ry) on a new map with ranges 0<=x<=8 and 0<=y<=4
+def pixelmap(px, py):
+    return 8 * (px - xmin) / xrange, 4 * (1 - (py - ymin) / yrange)
 
-for x in (0, xmin, xrange/2, xmax):
-    print(pixmapx(x))
 
-for y in (0, ymin, yrange/2, ymax):
-    print(pixmapy(y))
+ballpos = pixelmap(ball[0], ball[1])
+rtrianglepos = pixelmap(redteam['triangle'][0], redteam['triangle'][1])
+print("Real Ball Position: " + str(ballpos))
+print("Red Triangle Position: " + str(rtrianglepos))
